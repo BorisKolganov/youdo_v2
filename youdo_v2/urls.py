028a7c0f9984aaebc1d58b4628a8ed941d17e612
BorisKolganov <bkolganov@me.com>
2016-06-15 00:05:51 +0300

    add static f

--- youdo_v2/urls.py
+++ youdo_v2/urls.py
@@ -2,10 +2,11 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('core.urls',  namespace='core')),
     url(r'^', include('works.urls', namespace='works')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + staticfiles_urlpatterns()
